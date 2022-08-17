643474465ce75c3961a823701939c8632741ee27
Sam Brindle <brindlejsam@gmail.com>
2022-08-17 11:39:45 -0700

    added integration and created a main file to call all files at once

--- scraper.py
+++ scraper.py
@@ -1,4 +1,7 @@
 from playwright.sync_api import sync_playwright
+import create_json
+import requests
+import api
 
 import getpass
 
@@ -15,8 +18,8 @@ def run(playwright):
     browser = playwright.chromium.launch(headless=False, channel='chrome')
 
     # context = browser.new_context()
-    user_email = input('Please input your Canvas account email:')
-    password = getpass.getpass('Please input your Canvas password:')
+    user_email = "brindlejsam@gmail.com"  #input('Please input your Canvas account email:')
+    password = "0408Futur!" #getpass.getpass('Please input your Canvas password:')
     page = browser.new_page()
     page.goto("https://canvas.instructure.com/calendar")
     # page.goto("https://canvas.instructure.com/calendar#view_name=agenda&view_start=2022-07-18")
@@ -35,49 +38,52 @@ def run(playwright):
 
     global assignment_link
     assignment_link = page.query_selector('//p[@id="calendar-feed-box-lower"]/a')
-    print(assignment_link.get_attribute("href"))
+    assignment_link = assignment_link.get_attribute("href")
+    # response = requests.get(assignment_text)
+    # create_json.create_json(response.content)
 
     #Dictionary: key: title, value: score
     dic={}
 
-    page.goto("https://canvas.instructure.com/courses/4916427/grades")
-    with page.expect_navigation():
-        page.wait_for_timeout(15000)
-        graded_titles = page.query_selector_all("//tr[@class='student_assignment assignment_graded editable']/th[@class='title']/a")
-
-        graded_actual_scores = page.query_selector_all("//span[@class='original_score']")
-        graded_total_scores = page.query_selector_all("//tr[@class='student_assignment assignment_graded editable']//td[@class='possible points_possible']")
-
-        not_graded_titles = page.query_selector_all("//tr[@class='student_assignment editable']/th[@class='title']/a")
-        not_graded_total_scores = page.query_selector_all("//tr[@class='student_assignment editable']//td[@class='possible points_possible']")
-
-
-        #Statistics for score data
-        #stats = page.query_selector_all("//tr[@class='comments grade_details assignment_graded']//tbody")
-
-        for index in range(len(graded_titles)):
-            print("Title: ", graded_titles[index].text_content())
-            print(f"actual scores: {graded_actual_scores[index].text_content()}")
-            print(f"total scores: {graded_total_scores[index].text_content()}")
-
-        for index in range(len(not_graded_titles)):
-            print("title: ", not_graded_titles[index].text_content())
-            print("actual scores: 0")
-            print("total scores:", not_graded_total_scores[index].text_content())
-    
-        #titles = page.query_selector_all("//tbody")  # //div['agenda-event__time']
-        #print(titles)
-        # for i in titles:
-        #     # print(i.text_content())
-        #     i = i.text_content()
-        with open('./output.txt', 'w') as f:
-            for i in titles:
-                f.write(i.text_content() + "\n")
+    # page.goto("https://canvas.instructure.com/courses/4916427/grades")
+    # with page.expect_navigation():
+    #     page.wait_for_timeout(15000)
+    #     graded_titles = page.query_selector_all("//tr[@class='student_assignment assignment_graded editable']/th[@class='title']/a")
+    #
+    #     graded_actual_scores = page.query_selector_all("//span[@class='original_score']")
+    #     graded_total_scores = page.query_selector_all("//tr[@class='student_assignment assignment_graded editable']//td[@class='possible points_possible']")
+    #
+    #     not_graded_titles = page.query_selector_all("//tr[@class='student_assignment editable']/th[@class='title']/a")
+    #     not_graded_total_scores = page.query_selector_all("//tr[@class='student_assignment editable']//td[@class='possible points_possible']")
+    #
+    #
+    #     #Statistics for score data
+    #     #stats = page.query_selector_all("//tr[@class='comments grade_details assignment_graded']//tbody")
+    #
+    #     for index in range(len(graded_titles)):
+    #         print("Title: ", graded_titles[index].text_content())
+    #         print(f"actual scores: {graded_actual_scores[index].text_content()}")
+    #         print(f"total scores: {graded_total_scores[index].text_content()}")
+    #
+    #     for index in range(len(not_graded_titles)):
+    #         print("title: ", not_graded_titles[index].text_content())
+    #         print("actual scores: 0")
+    #         print("total scores:", not_graded_total_scores[index].text_content())
+    #
+    #     titles = page.query_selector_all("//tbody")  # //div['agenda-event__time']
+    #     #print(titles)
+    #     # for i in titles:
+    #     #     # print(i.text_content())
+    #     #     i = i.text_content()
+    #     with open('./output.txt', 'w') as f:
+    #         for i in titles:
+    #             f.write(i.text_content() + "\n")
 
     page.wait_for_timeout(10000)
 
     page.close()
     browser.close()
+
 
 #For future reference
 # def export_data(dic):
@@ -85,6 +91,9 @@ def run(playwright):
 #         for k, v in dic:
 #             f.write(f"{k}, {v}")
 
+
 if __name__ == '__main__':
     with sync_playwright() as playwright:
         run(playwright)
+        response = requests.get(assignment_link)
+        create_json.create_json(response.content.decode('utf-8'))
